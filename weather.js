3813799607f54fec92e519d888dd88f522de8c1d
sssssshhh <eyesh0011@gmail.com>
2021-05-30 16:03:53 +0900

    weather api, show weather info

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 const getClassWeather = document.querySelector(".weather");
-const myApikey = "6dabee778f485c7231462aba0b87caf1";
+const myApikey = "2ee8c98d98db389890d31fcec832db22";
 
 function getMyLatandLogSuccess(position){
-    getWeather(position.coords.latitude, position.coords.longitude);
+    getWeathers(position.coords.latitude, position.coords.longitude);
 }
 
 function getMyLatandLogFailure(position){
@@ -13,17 +13,52 @@ function getMyLatandLogFailure(position){
     navigator.geolocation.getCurrentPosition(getMyLatandLogSuccess, getMyLatandLogFailure);
  }
  
-function getWeather(lat, lon){
-    // todo. doseon't working,,why?
-    console.log(`${lat}:${lon}`);
-    fetch(`api.openweathermap.org/data/2.5/weather?q=London&appid=${myApikey}`).then(
-      function(response){
-         return console.log("success");
-      }
-    );
+function getWeathers(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&&units=metric&exclude=hourly,minutely&appid=${myApikey}`)
+    .then(response => response.json())
+    .then(j => showWeathers(j.daily));
 }
 
+function showWeathers(dailyWeather){
+    const table = document.createElement('table');
+    table.style.color = "pink";
+    table.style.backgroundColor = "#ffffff";
+    table.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    table.style.borderRadius = "30px";
 
+
+    for(daily of dailyWeather){
+        const tr = document.createElement('tr');
+        
+        // set date
+        const td_date = document.createElement('td');
+        td_date.innerText = new Date(daily.dt * 1000).toDateString();
+        tr.appendChild(td_date);
+        
+        // set icon
+        const td_icon = document.createElement('td');
+        const imgWeather = new Image();
+        imgWeather.src = `http://openweathermap.org/img/wn/${daily.weather[0].icon}@2x.png`;
+        imgWeather.height = 40;
+        imgWeather.width = 50;
+        td_icon.appendChild(imgWeather);
+        tr.appendChild(td_icon);
+        table.appendChild(tr);
+
+        // set temperature
+        const td_cel = document.createElement('td');
+        const maxTemp = Math.floor(daily.temp.max);
+        const minTemp = Math.floor(daily.temp.min);
+        td_cel.innerText = `${maxTemp}/${minTemp}℃`;
+        tr.appendChild(td_cel);
+
+        // set weather comment
+        const td_comment = document.createElement('td');
+        td_comment.innerText = daily.weather[0].description;
+        tr.appendChild(td_comment);
+    }
+    getClassWeather.appendChild(table);
+}
 
 function weather(){
     getMyLatandLog();
